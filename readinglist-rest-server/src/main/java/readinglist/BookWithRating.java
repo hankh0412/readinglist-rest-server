8f69e2c4e4ef9d721cbcd63746a7c9ec2ebb0871
ohgamja3 <ohgamja3@gmail.com>
2018-03-03 16:57:36 +0900

    Reformat Code

--- readinglist-rest-server/src/main/java/readinglist/BookWithRating.java
+++ readinglist-rest-server/src/main/java/readinglist/BookWithRating.java
@@ -2,7 +2,7 @@ package readinglist;
 
 import org.springframework.data.rest.core.config.Projection;
 
-@Projection(name = "BookWithRating", types = { Book.class })
+@Projection(name = "BookWithRating", types = {Book.class})
 interface BookWithRating {
 
     Float getRating();
