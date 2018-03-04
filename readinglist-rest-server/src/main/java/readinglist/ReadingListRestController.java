a007ef9b9eda4a5d4783d4cc97cab30021250a6e
ohgamja3 <ohgamja3@gmail.com>
2018-03-04 17:02:14 +0900

    Change rest url by adding swagger

--- readinglist-rest-server/src/main/java/readinglist/ReadingListRestController.java
+++ readinglist-rest-server/src/main/java/readinglist/ReadingListRestController.java
@@ -1,18 +1,14 @@
 package readinglist;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/books")
 class ReadingListRestController {
 
     @Autowired
