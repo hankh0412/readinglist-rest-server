6901437d77d828d69639f9d76e5b7ee968df5bf4
ohgamja3 <ohgamja3@gmail.com>
2019-01-21 09:09:26 +0900

    Chanaged folder structure

---
+++ src/main/java/readinglist/BookWithRating.java
@@ -0,0 +1,13 @@
+package readinglist;
+
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "BookWithRating", types = {Book.class})
+interface BookWithRating {
+
+    Float getRating();
+
+    String getReview();
+
+    Book getBook();
+}
